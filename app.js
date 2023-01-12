ed771dcce699f061966f36128a857eedaa412297
anne rubio <rubio.anne@gmail.com>
2023-01-12 17:57:24 +0100

    final commit

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// Changing color of list-cards * 3 to red on mouse over cf active state
-
+// Changing color of list-cards titles to red on mouse over cf active state
 const colored = document.querySelectorAll(".colored").forEach((colored)=> {
   colored.addEventListener("mouseover", (event)=> {
     event.currentTarget.classList.toggle("red");
   });
 });
 
-// Changing color of side-cards h3 to orange on mouse over cf active state
+// Changing color of side-cards titles to orange on mouse over cf active state
 const oranged = document.querySelectorAll('.oranged').forEach((oranged)=> {
   oranged.addEventListener("mouseover", (event)=> {
     event.currentTarget.classList.toggle("orange")
@@ -14,16 +13,13 @@ const oranged = document.querySelectorAll('.oranged').forEach((oranged)=> {
 });
 
 // Displaying or hiding mobile-sized navbar
-
-const open = document.getElementById('open');
-  open.addEventListener("click", (event)=> {
-    event.currentTarget.style.display= 'none';
-  });
-
-const close = document.querySelector('.close');
-  close.addEventListener("click", (event)=> {
-    event.currentTarget.style.display= 'none';
-  });
+const toggle = document.querySelector('.toggle');
+const body = document.querySelector('body');
+// on click on toogle buttons add and remove a class open on the body,
+// this way we know when the menu is opened thanks to this class
+toggle.addEventListener("click", (event) => {
+  body.classList.toggle("open");
+})
 
 // const element = document.querySelector(CSS_SELECTOR);
 // // Hide
